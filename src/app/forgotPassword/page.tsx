12df45f99bbdceb3303c9fc8d9feb91744263c13
ayla-imran851 <ayla@macworldtechnology.com>
2026-01-21 14:39:38 +0500

    ..

--- src/app/forgotPassword/page.tsx
+++ src/app/forgotPassword/page.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ForgotPass from "@/src/components/AppComponents/ForgotPass";
-const page = () => {
-  return (
-    <div>
-      <ForgotPass />
-    </div>
-  );
-};
+// import React from "react";
+// import ForgotPass from "@/src/components/AppComponents/ForgotPass";
+// const page = () => {
+//   return (
+//     <div>
+//       <ForgotPass />
+//     </div>
+//   );
+// };
 
-export default page;
+// export default page;

9a5c80a1a1bebce50feeafe5bacff67b9b923e33
ayla-imran851 <ayla@macworldtechnology.com>
2026-01-21 13:02:35 +0500

    car detail url fixed

--- src/app/auctionDetail/page.tsx
+++ src/app/auctionDetail/page.tsx
@@ -1,10 +1,13 @@
-import CarDetail from '@/src/components/AppComponents/CarDetail'
-import React from 'react'
+import AuctionDetailPage from "@/components/AppComponents/CarDetail";
+import { Car } from "lucide-react";
+import React from "react";
 
 const page = () => {
   return (
     <div>
-      <CarDetail />
+      <AuctionDetailPage />
     </div>
   );
-}
+};
+
+export default page;

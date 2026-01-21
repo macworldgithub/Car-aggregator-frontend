600dd79d5352a3457435cb48434bf3683dc01dfb
jawwadkareem <karimjawwad09@gmail.com>
2026-01-21 15:18:51 +0500

    ..

--- src/app/auctionCalender/page.tsx
+++ src/app/auctionCalender/page.tsx
@@ -1,14 +1,31 @@
-// import UpcomingAuctions from "@/src/components/AppComponents/UpcomingAuctions";
+// // import UpcomingAuctions from "@/src/components/AppComponents/UpcomingAuctions";
+// import UpcomingAuctions from "@/components/AppComponents/UpcomingAuctions";
+// import React from "react";
+// // import AuctionCalenderHero from "@/src/components/AppComponents/AuctionCalendarHero";
+// import AuctionCalenderHero from "@/components/AppComponents/AuctionCalendarHero";
+
+// const page = () => {
+//   return (
+//     <div>
+//       <AuctionCalenderHero />
+//       <UpcomingAuctions />
+//     </div>
+//   );
+// };
+
+// export default page;
+import React, { Suspense } from "react";
 import UpcomingAuctions from "@/components/AppComponents/UpcomingAuctions";
-import React from "react";
-// import AuctionCalenderHero from "@/src/components/AppComponents/AuctionCalendarHero";
 import AuctionCalenderHero from "@/components/AppComponents/AuctionCalendarHero";
 
 const page = () => {
   return (
     <div>
       <AuctionCalenderHero />
-      <UpcomingAuctions />
+
+      <Suspense fallback={<div>Loading auctions...</div>}>
+        <UpcomingAuctions />
+      </Suspense>
     </div>
   );
 };
